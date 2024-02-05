90bb028a35adb6bc1cc789a3c8790b2925373cb1
christina <158211764+christinac09@users.noreply.github.com>
2024-02-05 13:59:48 -0500

    2/5

--- data.py
+++ data.py
@@ -1,4 +1,33 @@
 #Data types
-x = 3
-y = float(3)
-print(x,y)
+# x = 3
+# y = float(3)
+# print(x,y)
+
+""" values = [1,2.23,5,7,2,30,15]
+print(values)
+for i in values:
+    print(i)
+print(values[0])
+print(values[6]) """
+""" 
+
+
+
+test = ["t","e","s","t"]
+for i in test:
+    print(i)
+
+x = "this is a thing"
+y = x.split( )              # y is a list with the elements being each separate word in x
+z = y[0]                    # z is the first element in y
+print(y)
+print(z) """
+
+
+message = "please input a sentence"        # word counter with user input
+print(message)
+input = input()
+words = input.split( )
+print(len(words))
+
+
---
+++ lists.py
@@ -0,0 +1,17 @@
+animals = ["Zebra", "Ape", "Freshman"]
+# print(animals[0])
+
+""" for animal in animals:
+    if(animal == "Ape"):
+        print(animal) """
+
+name = "Christina"
+# #print(name[0])
+# x = name.upper()
+# print(x)
+message = "Hello freshmen"
+words = message.split( )
+print(len(words))
+y = input("give me a number")
+print(y)
+print(int(y) + 9)
